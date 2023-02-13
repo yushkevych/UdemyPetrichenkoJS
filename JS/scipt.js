cf0f5e86163744946ea881b57e25aa86d5fc1d37
YVP <valerayu@gmail.com>
2023-02-13 09:54:36 +0200

    first commit

---
+++ JS/scipt.js
@@ -0,0 +1,44 @@
+"use strict";
+
+
+/* 
+let number = 5;
+
+const leftBorderWidth = 1;
+
+
+number = 10;
+console.log(number);
+
+
+let user = {
+  name: "Vasya"
+};
+
+let id = Symbol("id");
+user[id] = 5; 
+
+console.log(user[id]);
+
+const arr = [1, 2, 3]; */
+
+// const result = confirm("Please confirm your choice");
+// console.log(result);
+
+/* const answer = prompt("Are you 18?", "18");
+console.log(typeof(answer)); */
+/* const answers = [];
+
+answers[0] = prompt("What is your name?",'');
+answers[1] = prompt("What is your surname?",'');
+answers[2] = prompt("What is your age?",'');
+
+document.write(answers);
+ */
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`)
+
+
+
